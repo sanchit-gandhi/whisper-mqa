63458db12f010baf29de52a064d966ef31bb9de2
sanchit-gandhi <sanchit@huggingface.co>
2023-03-01 09:28:22 +0100

    fix cross-attn

--- whisper_mqa.py
+++ whisper_mqa.py
@@ -32,6 +32,10 @@ class WhisperMQAttention(nn.Module):
         self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
 
+    # Copied from transformers.models.whisper.modeling_whisper.WhisperAttention._shape
+    def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
+        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -50,7 +54,7 @@ class WhisperMQAttention(nn.Module):
         bsz, tgt_len, _ = hidden_states.size()
 
         # get query proj
-        query_states = self.q_proj(hidden_states) * self.scaling
+        query_states = self.q_proj(hidden_states) * self.scaling  # (bsz, tgt_len, embed_dim)
         # get key, value proj
         # `past_key_value[0].shape[2] == key_value_states.shape[1]`
         # is checking that the `sequence_length` of the `past_key_value` is the same as
@@ -65,15 +69,18 @@ class WhisperMQAttention(nn.Module):
             value_states = past_key_value[1]
         elif is_cross_attention:
             # cross_attentions
-            key_states, value_states = self.kv_proj(key_value_states).split(self.head_dim, dim=2)
+            key_value_states = self.kv_proj(key_value_states)
+            key_states, value_states = key_value_states.split(self.head_dim, dim=2)  # (bsz, src_len, head_dim)
         elif past_key_value is not None:
             # reuse k, v, self_attention
-            key_states, value_states = self.kv_proj(key_value_states).split(self.head_dim, dim=2)
+            key_value_states = self.kv_proj(hidden_states)
+            key_states, value_states = key_value_states.split(self.head_dim, dim=2)  # (bsz, src_len, head_dim)
             key_states = torch.cat([past_key_value[0], key_states], dim=2)
             value_states = torch.cat([past_key_value[1], value_states], dim=2)
         else:
             # self_attention
-            key_states, value_states = self.kv_proj(hidden_states).split(self.head_dim, dim=2)  # (bsz, seq_len, head_dim)
+            key_value_states = self.kv_proj(hidden_states)
+            key_states, value_states = key_value_states.split(self.head_dim, dim=2)  # (bsz, seq_len, head_dim)
 
         if self.is_decoder:
             # if cross_attention save Tuple(torch.Tensor, torch.Tensor) of all cross attention key/value_states.
@@ -85,30 +92,30 @@ class WhisperMQAttention(nn.Module):
             # if encoder bidirectional self-attention `past_key_value` is always `None`
             past_key_value = (key_states, value_states)
 
-        # (bsz, heads, src_len, head_dim) -> (batch, heads * src_length, head_dim)
+        # (bsz, tgt_len, embed_dim) -> (bsz, heads, tgt_len, head_dim)
+        query_states = self._shape(query_states, tgt_len, bsz)
+        # (bsz, heads, tgt_len, head_dim) -> (batch, heads * tgt_len, head_dim)
         query_states = query_states.reshape(bsz, self.num_heads * tgt_len, self.head_dim)
-        key_states = key_states.permute(0, 2, 1)  # (batch_size, head_dim, tgt_length)
-        # value (batch_size, tgt_length, head_dim)
 
+        # key/value: (bsz, src_len, head_dim)
         src_len = key_states.size(1)
-        query_length = query_states.size(1) // self.num_heads
-        key_length = key_states.size(2)
 
-        # (bsz, num_heads * src_len, head_dim) x (bsz, head_dim, tgt_len) -> (b, num_heads * src_len, tgt_len)
-        attn_weights = torch.bmm(query_states, key_states)
-        # -> (bsz, num_heads, src_len, tgt_len)
-        attn_weights = attn_weights.view(bsz, self.num_heads, query_length, key_length)
+        # (bsz, num_heads * tgt_len, head_dim) x (bsz, head_dim, src_len) -> (bsz, num_heads * tgt_len, src_len)
+        attn_weights = torch.bmm(query_states, key_states.transpose(1, 2))
 
-        if attn_weights.size() != (bsz, self.num_heads, tgt_len, src_len):
+        if attn_weights.size() != (bsz, self.num_heads * tgt_len, src_len):
             raise ValueError(
-                f"Attention weights should be of size {(bsz, self.num_heads, tgt_len, src_len)}, but is"
-                f" {attn_weights.size()}"
+                f"Attention weights should be of size {(bsz, self.num_heads * tgt_len, src_len)}, but is"
+                f" {attn_weights.size()}."
             )
+
+        # -> (bsz, num_heads, tgt_len, src_len)
+        attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
 
         if attention_mask is not None:
             if attention_mask.size() != (bsz, 1, tgt_len, src_len):
                 raise ValueError(
-                    f"Attention mask should be of size {(bsz, 1, tgt_len, src_len)}, but is {attention_mask.size()}"
+                    f"Attention mask should be of size {(bsz, 1, tgt_len, src_len)}, but is {attention_mask.size()}."
                 )
             attn_weights = attn_weights + attention_mask
 
@@ -118,39 +125,35 @@ class WhisperMQAttention(nn.Module):
             if layer_head_mask.size() != (self.num_heads,):
                 raise ValueError(
                     f"Head mask for a single layer should be of size {(self.num_heads,)}, but is"
-                    f" {layer_head_mask.size()}"
+                    f" {layer_head_mask.size()}."
                 )
             attn_weights = layer_head_mask.view(1, -1, 1, 1) * attn_weights
 
         if output_attentions:
-            # this operation is a bit awkward, but it's required to
-            # make sure that attn_weights keeps its gradient.
-            # In order to do so, attn_weights have to be reshaped
-            # twice and have to be reused in the following
             attn_weights_output = attn_weights
         else:
             attn_weights_output = None
 
         attn_probs = nn.functional.dropout(attn_weights, p=self.dropout, training=self.training)
 
-        # (bsz, num_heads, src_len, tgt_len) -> (bsz, num_heads * src_len, tgt_len)
-        attn_probs = attn_probs.view(bsz, self.num_heads * query_length, key_length)
-        # (bsz, num_heads * src_len, tgt_len) x (bsz, tgt_len, head_dim) -> (bsz, num_heads * src_len, head_dim)
+        # (bsz, num_heads, tgt_len, src_len) -> (bsz, num_heads * tgt_len, src_len)
+        attn_probs = attn_probs.view(bsz, self.num_heads * tgt_len, src_len)
+        # (bsz, num_heads * tgt_len, src_len) x (bsz, src_len, head_dim) -> (bsz, num_heads * tgt_len, head_dim)
         attn_output = torch.bmm(attn_probs, value_states)
 
         if attn_output.size() != (bsz, self.num_heads * tgt_len, self.head_dim):
             raise ValueError(
                 f"`attn_output` should be of size {(bsz, self.num_heads * tgt_len, self.head_dim)}, but is"
-                f" {attn_output.size()}"
+                f" {attn_output.size()}."
             )
 
-        attn_output = attn_output.view(bsz, self.num_heads, query_length, self.head_dim)
+        attn_output = attn_output.view(bsz, self.num_heads, tgt_len, self.head_dim)
         attn_output = attn_output.transpose(1, 2)
 
         # Use the `embed_dim` from the config (stored in the class) rather than `hidden_state` because `attn_output` can be
         # partitioned across GPUs when using tensor-parallelism.
         attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
 
-        attn_output = self.out_proj(attn_output)
+        attn_output = self.out_proj(attn_output)  # (bsz, tgt_len, embed_dim)
 
         return attn_output, attn_weights_output, past_key_value
